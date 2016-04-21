5ab0601f6ff6357b1806ce1c8eaa9f6ba99764da
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-21 11:50:34 -0700

    Work on user input and flow between activitites

--- app/src/main/java/com/example/guest/cofeshop/MainActivity.java
+++ app/src/main/java/com/example/guest/cofeshop/MainActivity.java
@@ -3,24 +3,29 @@ package com.example.guest.cofeshop;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-
     private Button mFindShopsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindShopsButton = (Button) findViewById(R.id.findShopsButton);
         mFindShopsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, CoffeeShopsActivity.class);
                 startActivity(intent);
             }
