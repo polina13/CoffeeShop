b1acf32a262f646c17334db47d1005e28b0eed19
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-22 09:42:34 -0700

    Add model

--- app/src/main/java/com/example/guest/cofeshop/MainActivity.java
+++ app/src/main/java/com/example/guest/cofeshop/MainActivity.java
@@ -6,29 +6,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
+import com.example.guest.cofeshop.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindShopsButton;
-//    private EditText mLocationEditText;
+    @Bind(R.id.findCoffeeShopsButton)
+    Button mFindShopsButton;
 
+//    private Button mFindShopsButton;
+//    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
 //        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindShopsButton = (Button) findViewById(R.id.findShopsButton);
-//        mFindShopsButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
+        mFindShopsButton.setOnClickListener(new View.OnClickListener() {
+            ;
+
+            @Override
+            public void onClick(View v) {
 //                String location = mLocationEditText.getText().toString();
-//                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, CoffeeShopsActivity.class);
                 startActivity(intent);
             }
-
+        });
+    }
 }
