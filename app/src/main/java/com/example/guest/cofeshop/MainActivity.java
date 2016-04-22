dc2a221e42559aa8f8249e5b4353fecc8081bbb3
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-22 09:06:26 -0700

    Update gradle build file

--- app/src/main/java/com/example/guest/cofeshop/MainActivity.java
+++ app/src/main/java/com/example/guest/cofeshop/MainActivity.java
@@ -12,24 +12,23 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindShopsButton;
-    private EditText mLocationEditText;
+//    private EditText mLocationEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindShopsButton = (Button) findViewById(R.id.findShopsButton);
-        mFindShopsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
+//        mFindShopsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String location = mLocationEditText.getText().toString();
+//                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, CoffeeShopsActivity.class);
                 startActivity(intent);
             }
-        });
 
-    }
 }
