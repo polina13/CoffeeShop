8763b3e379b405869ac48eeaab8cad007a960cf8
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-29 08:58:25 -0700

    Set up gradle.properties and add YELP Api to gradle.build

---
+++ app/src/main/java/com/example/guest/cofeshop/Constants.java
@@ -0,0 +1,11 @@
+package com.example.guest.cofeshop;
+
+/**
+ * Created by alexnenchev on 4/29/16.
+ */
+public class Constants {
+    public static final String CONSUMER_KEY = BuildConfig.CONSUMER_KEY;
+    public static final String CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET;
+    public static final String TOKEN = BuildConfig.TOKEN;
+    public static final String TOKEN_SECRET = BuildConfig.TOKEN_SECRET;
+}
