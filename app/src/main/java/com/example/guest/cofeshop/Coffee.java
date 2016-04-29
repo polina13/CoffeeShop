6d9e89800a25c0e906104aee35178e4714b4efc1
Polina Nenchev <polina.mihova@yahoo.com>
2016-04-29 11:58:14 -0700

    Edit the coffee model and input json data

--- app/src/main/java/com/example/guest/cofeshop/Coffee.java
+++ app/src/main/java/com/example/guest/cofeshop/Coffee.java
@@ -2,19 +2,81 @@ package com.example.guest.cofeshop;
 
 import java.util.ArrayList;
 
-/**
- * Created by alexnenchev on 4/29/16.
- */
 public class Coffee {
     private String mName;
     private String mPhone;
     private String mWebsite;
-    private String mFacebook;
     private String mImage;
-    private String mReview;
+    private double mRating;
+//    private String mComments;
     private ArrayList<String> mAddress = new ArrayList<>();
     private String mCategories;
+    private String mMenu;
+//    private String mBrand; //check JSON FILE as well
+    private int mReviewCount;
 
     public Coffee() {}
 
+    public Coffee(String name, String phone, String website, String image, double rating, ArrayList<String> address, String categories, String menu, int reviewCount ) {
+
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        mImage = getLargeImage(image);
+        this.mRating = rating;
+        this.mAddress = address;
+        this.mCategories = categories;
+        this.mMenu = menu;
+        this.mReviewCount = reviewCount;
+
+
+    }
+
+    private String getLargeImage(String image) {
+        return mImage;
+    }
+
+    public String getName () {
+            return mName;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+
+    public String getWebsite() {
+        return mWebsite;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public String getCategories(String categories) {
+        if (categories == "coffeeroasteries") {
+        }
+        return mCategories;
+    }
+
+    public String getMenu() {
+        return mMenu;
+    }
+
+    public int getReviewCount() {
+        return mReviewCount;
+    }
 }
+
+
+//NOTES TO DO
+//LOOK UP FOR REVIEWS/COMMENTS AND PRICES
--- app/src/main/java/com/example/guest/cofeshop/YelpService.java
+++ app/src/main/java/com/example/guest/cofeshop/YelpService.java
@@ -1,10 +1,18 @@
 package com.example.guest.cofeshop;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
@@ -35,5 +43,45 @@ public class YelpService {
         Call call = client.newCall(request);
         call.enqueue(callback);
     }
+
+    public ArrayList<Coffee> processResults(Response response){
+        ArrayList<Coffee> coffeesShops = new ArrayList<>();
+
+        try {
+            String jsonYelpData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject yelpSearchJSON = new JSONObject(jsonYelpData);
+                JSONArray businessesJSON = yelpSearchJSON.getJSONArray("businesses");
+                for (int i = 0; i < businessesJSON.length(); i++) {
+                    JSONObject coffeeShopJSON = businessesJSON.getJSONObject(i);
+                    String name = coffeeShopJSON.getString("name");
+                    String phone = coffeeShopJSON.optString("display_phone", "Sorry, phone not available");
+                    String website = coffeeShopJSON.getString("url");
+                    String image = coffeeShopJSON.getString("image_url");
+                    double rating = coffeeShopJSON.getDouble("rating");
+                    ArrayList<String> address = new ArrayList<>();
+                    JSONArray addressJSON = coffeeShopJSON.getJSONObject("location").getJSONArray("display_address");
+                    for (int y = 0; y < addressJSON.length(); y++) {
+                        address.add(addressJSON.get(y).toString());
+                    }
+                    String categories = coffeeShopJSON.getString("categories");
+//                    double latitude = restaurantJSON.getJSONObject("location")
+//                            .getJSONObject("coordinate").getDouble("latitude");
+//                    double longitude = restaurantJSON.getJSONObject("location")
+//                            .getJSONObject("coordinate").getDouble("longitude");
+                    String menu = coffeeShopJSON.optString("menu_provider", "Menu provider not included");
+                    int reviewCount = coffeeShopJSON.getInt("review_count");
+                    Coffee coffee = new Coffee(name, phone, website, image, rating, address, categories, menu, reviewCount);
+                    coffeesShops.add(coffee);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return coffeesShops;
+    }
 }
+
 
