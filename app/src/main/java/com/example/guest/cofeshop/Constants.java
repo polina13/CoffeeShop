9a259fdef2414c6803e9d71f0ae98d8211bf3bc1
Polina Nenchev <polina.mihova@yahoo.com>
2016-05-06 10:20:26 -0700

    create app in firease and set up the firebase root url in the coffeeshop app

--- app/src/main/java/com/example/guest/cofeshop/Constants.java
+++ app/src/main/java/com/example/guest/cofeshop/Constants.java
@@ -13,4 +13,6 @@ public class Constants {
     public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=coffee";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
 
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+
 }
---
+++ app/src/main/java/com/example/guest/cofeshop/services/User.java
@@ -0,0 +1,7 @@
+package com.example.guest.cofeshop.services;
+
+/**
+ * Created by alexnenchev on 5/6/16.
+ */
+public class User {
+}
