6e8bd62dabdd7fdbde68fbcc5c2736a13e50fde5
Polina Nenchev <polina.mihova@yahoo.com>
2016-05-06 12:37:47 -0700

    set up the firebase recycler view

---
+++ app/src/main/java/com/example/guest/cofeshop/ui/SavedCoffeeShopsListActivity.java
@@ -0,0 +1,47 @@
+package com.example.guest.cofeshop.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.guest.cofeshop.Coffee;
+import com.example.guest.cofeshop.Constants;
+import com.example.guest.cofeshop.R;
+import com.example.guest.cofeshop.adapters.FirebaseCoffeeShopListAdapter;
+import com.firebase.client.Firebase;
+import com.firebase.client.Query;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SavedCoffeeShopsListActivity extends AppCompatActivity {
+    private Query mQuery;
+    private Firebase mFirebaseCoffeeShopsRef;
+    private FirebaseCoffeeShopListAdapter mAdapter;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+        ButterKnife.bind(this);
+
+        mFirebaseCoffeeShopsRef = new Firebase(Constants.FIREBASE_URL_COFFEESHOPS);
+
+        setUpFirebaseQuery();
+        setUpRecyclerView();
+    }
+
+    private void setUpFirebaseQuery() {
+        String location = mFirebaseCoffeeShopsRef.toString();
+        mQuery = new Firebase(location);
+    }
+
+    private void setUpRecyclerView() {
+        mAdapter = new FirebaseCoffeeShopListAdapter(mQuery, Coffee.class);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
