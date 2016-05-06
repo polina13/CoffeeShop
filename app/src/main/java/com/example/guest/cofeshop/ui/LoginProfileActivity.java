b9dfef9f044f46577d5d312dca25fb12c32f0e05
Polina Nenchev <polina.mihova@yahoo.com>
2016-05-06 16:57:45 -0700

    set up login and sing up for profile activity and layouts

--- app/src/main/java/com/example/guest/cofeshop/ui/LoginProfileActivity.java
+++ app/src/main/java/com/example/guest/cofeshop/ui/LoginProfileActivity.java
@@ -1,15 +1,33 @@
 package com.example.guest.cofeshop.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.guest.cofeshop.R;
 
-public class LoginProfileActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginProfileActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_profile);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+        Intent intent = new Intent(LoginProfileActivity.this, CreateProfileActivity.class);
+        startActivity(intent);
+        finish();
+        }
     }
 }
