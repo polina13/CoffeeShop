ddd275f14394664d935c80d04cab25befe65b7bc
Polina Nenchev <polina.mihova@yahoo.com>
2016-05-06 16:50:29 -0700

    refactor manifest file to include intent-filter

--- app/src/main/java/com/example/guest/cofeshop/Constants.java
+++ app/src/main/java/com/example/guest/cofeshop/Constants.java
@@ -18,4 +18,8 @@ public class Constants {
     public static final String FIREBASE_LOCATION_COFFEESHOPS = "coffeeShops";
     public static final String FIREBASE_URL_COFFEESHOPS = FIREBASE_URL + "/" + FIREBASE_LOCATION_COFFEESHOPS;
 
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_PROPERTY_EMAIL = "email";
+    public static final String KEY_UID = "UID";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
 }
