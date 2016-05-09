cec33dbb0fcbb29a8d8d7472ffd21f1366fcbadb
Polina Nenchev <polina.mihova@yahoo.com>
2016-05-09 10:39:04 -0700

    edit and set up login profile functionality

--- app/src/main/java/com/example/guest/cofeshop/Constants.java
+++ app/src/main/java/com/example/guest/cofeshop/Constants.java
@@ -12,14 +12,18 @@ public class Constants {
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
     public static final String PREFERENCES_LOCATION_KEY = "location";
 
-    public static final String FIREBASE_LOCATION_SEARCHED_LOCATION = "searchedLocation";
     public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+
+    public static final String FIREBASE_LOCATION_SEARCHED_LOCATION = "searchedLocation";
     public static final String FIREBASE_URL_SEARCHED_LOCATION = FIREBASE_URL + "/" + FIREBASE_LOCATION_SEARCHED_LOCATION;
+
     public static final String FIREBASE_LOCATION_COFFEESHOPS = "coffeeShops";
     public static final String FIREBASE_URL_COFFEESHOPS = FIREBASE_URL + "/" + FIREBASE_LOCATION_COFFEESHOPS;
 
     public static final String FIREBASE_LOCATION_USERS = "users";
-    public static final String FIREBASE_PROPERTY_EMAIL = "email";
-    public static final String KEY_UID = "UID";
     public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+
+    public static final String KEY_UID = "UID";
+    public static final String KEY_USER_EMAIL = "email";
+
 }
