a106fa1a843bd3e1b83c424c02083c50f7ab400d
Polina Nenchev <polina.mihova@yahoo.com>
2016-05-13 11:44:37 -0700

    add  base fragment

---
+++ app/src/main/java/com/example/guest/coffeeShop/ui/BaseFragment.java
@@ -0,0 +1,35 @@
+package com.example.guest.coffeeShop.ui;
+
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+
+import com.example.guest.coffeeShop.Constants;
+import com.firebase.client.Firebase;
+
+public class BaseFragment extends Fragment {
+    public SharedPreferences mSharedPreferences;
+    public SharedPreferences.Editor mSharedPreferencesEditor;
+    public Firebase mFirebaseRef;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+    }
+
+    public void addToSharedPreferences(String location) {
+        mSharedPreferencesEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    }
+
+    public void logout() {
+        mFirebaseRef.unauth();
+        Intent intent = new Intent(getActivity(), LoginProfileActivity.class);
+        startActivity(intent);
+    }
+}
